eec5eb531431c2c1bd4dfd4448cc13fcf7fca86e
Nikhil TK <nikhiltk1114@gmail.com>
2022-12-04 16:39:10 +0530

    test for jenkins

--- src/main/java/com/example/jenkinsdemo/JenkinsdemoApplication.java
+++ src/main/java/com/example/jenkinsdemo/JenkinsdemoApplication.java
@@ -8,6 +8,7 @@ public class JenkinsdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsdemoApplication.class, args);
+		System.out.println("Jenkins is working");
 	}
 
 }
---
+++ src/main/java/com/example/jenkinsdemo/test/TestJenkins.java
@@ -0,0 +1,10 @@
+package com.example.jenkinsdemo.test;
+
+public class TestJenkins {
+
+	public static void main(String[] args) {
+		System.out.println("Jenkins Running fine!!");
+
+	}
+
+}
